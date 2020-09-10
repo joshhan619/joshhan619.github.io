0402500122acc3e2614131e06a1dd5cc61254efa
joshhan619 <joshhan619@gmail.com>
2020-09-10 15:34:12 -0400

    Updates

---
+++ precache-manifest.36c54ffec79d6d5325784f96bd304f89.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7bddaa56a336e1b03cd86d6f568225d5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fb4ac2a02472464bcca0",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "556581c83a9188c6a6df",
+    "url": "/static/js/2.0442b693.chunk.js"
+  },
+  {
+    "revision": "fb4ac2a02472464bcca0",
+    "url": "/static/js/main.da267a60.chunk.js"
+  },
+  {
+    "revision": "cf126c17722ea88ce4b6",
+    "url": "/static/js/runtime-main.46f0fd8b.js"
+  },
+  {
+    "revision": "e684fb4467b256a1c4052bd2b8c6f4b6",
+    "url": "/static/media/homescreen.e684fb44.jpg"
+  },
+  {
+    "revision": "5aca9881f999f84e76284225715549ad",
+    "url": "/static/media/josh.5aca9881.jpg"
+  }
+]);
