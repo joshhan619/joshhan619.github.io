cda23134c3a2fe4f8a2d60ea3dab65bd7db7dcdf
joshhan619 <joshhan619@gmail.com>
2020-10-14 10:30:50 -0400

    Updated resume

--- src/react-components/Resume.js
+++ src/react-components/Resume.js
@@ -52,33 +52,51 @@ function Resume() {
         <Divider/>
         <Text fontWeight="bold">Research Intern at Ryerson University CASPAL</Text>
         <Text fontStyle="italic" mb={2}>June 10, 2019 – August 31, 2019</Text>
-        <Text>Worked under Dr. Xiao-Ping Zhang on a research project in the area of signal/data information processing algorithms and applications. During this project I debugged code, wrote documentation, and developed a desktop app to download daily stock and futures data from IQFeed.
+        <Text>Worked under Dr. Xiao-Ping Zhang on a research project in the area of signal/data information processing algorithms and applications. During this project I debugged code, wrote documentation, and developed a Java web app to download daily stock and futures data from IQFeed. This allowed the research team to query thousands of 1-minute and daily historical ticker data, fundamental data on US stocks, and real-time tick-by-tick data on US and Canadian equities.
         </Text>
         <Heading variant='spaced_heading'>Projects</Heading>
         <Divider/>
         <Text fontWeight="bold" mb={2}>Interactive Webpage for Phonology Topic Quizzes</Text>
-        <Text mb={2}>In a team of two other students, I created a React website that implements a secure account system. The web application is an online quiz system designed for students or any visitor to practice phonology problem sets, as well as for professors to generate questions and track students’ learning progress. This project uses the Express.js web framework, Material-Ui, Mongoose.js and React Router libraries.
+        <Text mb={2}>I created a React website that implements a secure account system. The web application is an online quiz system designed for students or any visitor to practice phonology problem sets, as well as for professors to generate questions and track students’ learning progress. This project uses the Express.js web framework, Material-Ui, Mongoose.js and React Router libraries. The project is hosted on <a href="https://accelsnow.com/">https://accelsnow.com/</a>.
         </Text>
+        <Heading variant='spaced_heading'>Hackathons</Heading>
+        <Divider/>
+        <Text fontWeight="bold">2019 Chengdu80 Global Financial Product Design and R&D competition</Text>
+        <Text mb={2}> Won Best Innovator award for a novel research search tool in an international fintech competition hosted in Chengdu, China. </Text>
+        <Text fontWeight="bold">2020 UofTHacks</Text>
+        <Text mb={2}>Won best Lifestyle Hack for developing a community focused dieting and fitness app.</Text>
+
         <Heading variant='spaced_heading'>Skills</Heading>
         <Divider/>
         <ul>
           <li>Excellent organizational skills</li>
           <li>Strong communication and team-working skills</li>
-          <li>Experienced in using these frameworks and libraries:</li>
+          <li>Experienced in:</li>
           <ul>
-            <li>Express.js and Node.js</li>
+            <li>ExpressJS</li>
+            <li>ReactJS</li>
             <li>Bootstrap</li>
             <li>Material-UI</li>
-            <li>MongoDB and Mongoose.js</li>
-            <li>mySQL</li>
+            <li>CircleCI</li>
+            <li>PostgreSQL</li>
           </ul>
-          <li>Proficient in using these tools:</li>
+          <li>Proficient in:</li>
           <ul>
             <li>Microsoft Office (Word, Excel, PowerPoint)</li>
             <li>Adobe Photoshop</li>
             <li>Git</li>
             <li>Android Studio</li>
           </ul>
+        </ul>
+        <Heading variant='spaced_heading'>Relevant Courses</Heading>
+        <Divider/>
+        <Text>CGPA: 3.40</Text>
+        <ul>
+          <li>CSC309 Programming on the Web (A+)</li>
+          <li>CSC311 Intro to Machine Learning (A+)</li>
+          <li>CSC369 Operating Systems (A)</li>
+          <li>CSC418 Computer Graphics (A+)</li>
+          <li>CSC420 Intro to Image Understanding (A+)</li>
         </ul>
       </Box>
     </Flex>
