bb05f9d58485daa61387c7fde2461d649d485864
joshhan619 <joshhan619@gmail.com>
2020-10-14 10:32:10 -0400

    Updates

--- precache-manifest.9e15d8b4f9242640acf38ce8a0a35ca5.js
+++ precache-manifest.9e15d8b4f9242640acf38ce8a0a35ca5.js
@@ -1,27 +1,27 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "158da3377ff392b3dcf8efc6d1cda293",
+    "revision": "25ef2f9b11351a32722d84286c590f78",
     "url": "/index.html"
   },
   {
-    "revision": "9f1cd0d5a81a9a3ab96b",
+    "revision": "202f21bfa6c9c99770b4",
     "url": "/static/css/main.5f361e03.chunk.css"
   },
   {
-    "revision": "ba1de298b751bd71244a",
-    "url": "/static/js/2.27206f30.chunk.js"
+    "revision": "11ad0210fe6146486310",
+    "url": "/static/js/2.8bd928a1.chunk.js"
   },
   {
-    "revision": "9f1cd0d5a81a9a3ab96b",
-    "url": "/static/js/main.9f5cc190.chunk.js"
+    "revision": "202f21bfa6c9c99770b4",
+    "url": "/static/js/main.6bcbdce1.chunk.js"
   },
   {
     "revision": "cf126c17722ea88ce4b6",
     "url": "/static/js/runtime-main.46f0fd8b.js"
   },
   {
-    "revision": "cca59796a57d6d29a052c066da8c618b",
-    "url": "/static/media/Joshua Han Resume.cca59796.pdf"
+    "revision": "c6c0a5769f8905d6b2f5ff1bc7f1bc37",
+    "url": "/static/media/Joshua Han Resume.c6c0a576.pdf"
   },
   {
     "revision": "e684fb4467b256a1c4052bd2b8c6f4b6",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.95fb2ee7a66e0b5536a2632364986f69.js"
+  "/precache-manifest.9e15d8b4f9242640acf38ce8a0a35ca5.js"
 );
 
 self.addEventListener('message', (event) => {
